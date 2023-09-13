d1948c13278cbd63c0f224fbb934b2d0f845aa96
Geming Zhu <zgming20@163.com>
2023-09-13 03:15:19 -0400

    Finish alpha 2
    Finish alpha 2

--- Assets/Scripts/TitleScript.cs
+++ Assets/Scripts/TitleScript.cs
@@ -1,27 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using static UnityEngine.EventSystems.EventTrigger;
 
 public class TitleScript : MonoBehaviour
 {
-    private GUIStyle buttonStyle;
-    private void OnGUI()
+    public void start()
     {
-        GUILayout.BeginArea(new Rect(10, Screen.height / 2 + 100, Screen.width - 10, 200));
-        if (GUILayout.Button("New Game"))
-        {
-            Application.LoadLevel("BasicSpaceInvader");
-        }
-        if (GUILayout.Button("High score"))
-        {
-            Debug.Log("You should implement a high score screen.");
-        }
-        if (GUILayout.Button("Exit"))
-        {
-            Application.Quit();
-            Debug.Log("Application.Quit() only works in build,not in editor");
-        }
-        GUILayout.EndArea();
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(1);
     }
 }
